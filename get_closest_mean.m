84bcdf878b3c0013ffeebacee40d0e02c2daa911
Aleksejs Loginovs <ALoginovs@dundee.ac.uk>
2018-11-08 21:59:00 +0000

    fiddling with closest mean calculation

--- get_closest_mean.m
+++ get_closest_mean.m
@@ -2,40 +2,47 @@
 function y = get_closest_mean(all_means, image_mean)
     [rows, cols] = size(all_means);
     
-    
-    min_dist=all_means(1)+all_means(1)+all_means(1);
-    min_id=1;
+    min_dist=1000;
+    min_id=0;
     
     counter=1;
     id=[];
     
-    for i=2:rows
+    %calculate weights for colours based on image mean
+    %colour with largest value will have largest weight
+    %color with lowest value will have lowest weight
+    weights = rescale(image_mean,1,3).^2;%bring numbers to 1-2 range
+    %make sure there are no 0 in weights
+    %weights(weights==0)=1;
+    
+    for i=1:rows
             %i+rows and i+rows*2 instead of i+1 and i+2 is because it goes through
             %rows first all_means(i+1) would be the value in the same column next row
             cur_mean=[all_means(i), all_means(i+rows),all_means(i+rows*2)];
             
-            dist = abs(image_mean-cur_mean);
-            simple_dist=dist(1)+dist(2)+dist(3);
+            dist = abs(cur_mean - image_mean);
+            %simple_dist=dist(1)+dist(2)+dist(3);
+            weighted_dist = dist(1)/weights(1) + dist(2)/weights(2) + dist(3)/weights(3);            
             
-            if simple_dist < 3 && simple_dist < min_dist 
-                id(counter)=i;
-                min_dist = simple_dist;
+%             if weighted_dist < 3 && weighted_dist < min_dist 
+%                 id(counter)=i;
+%                 min_dist = weighted_dist;
+%                 min_id=i;
+%                 counter=counter+1;
+%             elseif weighted_dist < 3
+%                 id(counter)=i;
+%                 counter=counter+1;
+%             else
+            if weighted_dist < min_dist
+                min_dist = weighted_dist;
                 min_id=i;
-                counter=counter+1;
-            elseif simple_dist < 15
-                id(counter)=i;
-                counter=counter+1;
-            else
-                if simple_dist < min_dist
-                min_dist = simple_dist;
-                min_id=i;
-            end     
+            end
+            
     end
     if numel(id)==0
         y = min_id;
     else
         rand=randi([1,counter-1]);
-        id
-        y=id(rand);
+        y=id(rand)
     end
 end
