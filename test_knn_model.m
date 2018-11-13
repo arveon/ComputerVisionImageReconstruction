c809b62e8c3380bbe32d7339e9711d401fe5c8bd
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2018-11-13 16:13:17 +0000

    Generalised the test func

--- test_knn_model.m
+++ test_knn_model.m
@@ -1,11 +1,11 @@
-function percent = test_knn_model(mdl, test_means, expected_labels)
-    amount = size(test_means);
+function percent = test_knn_model(mdl, features, expected_labels)
+    amount = size(features);
     amount = amount(1);
     count = 1;
     correct = 0;
     incorrect = 0;
     while count<=amount
-        [label score cost] = predict(mdl, test_means(count));
+        [label score cost] = predict(mdl, features(count));
         if(strcmp(label, expected_labels(count))==1)
            correct = correct + 1;
         else
