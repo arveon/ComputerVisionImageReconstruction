68411f1bc0c3fefe6b326e478129aff3b189dc4a
Rony Portelli <portelli.rony@gmail.com>
2018-11-19 20:37:32 +0000

    change the feature edges, achieved 0.86 acc

--- controller.m
+++ controller.m
@@ -3,49 +3,42 @@ controller.prepare = @load_features;
 controller.train = @train;
 controller.predict_manmade = @predict_manmade;
 controller.predict_natural = @predict_natural;
+controller.generate_files = @generate_files;
 
 end
-function [edges,gabor,labels] = load_features()
+function generate_files()
 features_prep = knn_preTrainning();
-edges = features_prep.load_features_from_file("./modified/edge.data");
-gabor = features_prep.load_features_from_file("./modified/gabor.data");
-labels = [features_prep.generate_labels("natural_trainning","natural");features_prep.generate_labels("manmade_trainning","manmade")];
-
+features_prep.edge_feature("natural_trainning","feature_natural.data");
+features_prep.edge_feature("manmade_trainning","feature_manmade.data");
+features_prep.edge_feature("natural_testing","feature_natural_testing.data");
+features_prep.edge_feature("manmade_testing","feature_manmade_testing.data");
+end
+function [feature_trainning,feature_natural_testing,feature_manmade_testing,label_trainning,label_testing_natural,label_testing_manmade] = load_features()
+features_prep = knn_preTrainning();
+feature_trainning = [features_prep.load_features_from_file("./modified/feature_natural.data");features_prep.load_features_from_file("./modified/feature_manmade.data")];
+feature_natural_testing = features_prep.load_features_from_file("./modified/feature_natural_testing.data");
+feature_manmade_testing = features_prep.load_features_from_file("./modified/feature_manmade_testing.data");
+label_trainning = [features_prep.generate_labels("natural_trainning","natural"),features_prep.generate_labels("manmade_trainning","manmade")];
+label_testing_natural = features_prep.generate_labels("natural_testing","natural");
+label_testing_manmade = features_prep.generate_labels("manmade_testing","manmade");
 end
 function y = train(features,labels)
-y = fitcknn(features,labels,'NumNeighbors',3,'Standardize',1);
+%y = fitcknn(features,labels,'NumNeighbors',5,"Standardize",1);
+y= fitcknn(features,labels,'OptimizeHyperparameters','auto',...
+    'HyperparameterOptimizationOptions',...
+    struct('AcquisitionFunctionName','expected-improvement-plus'))
 end
 function y = predict_manmade(model, features)
-amount = size(features);
-    amount = amount(1);
-    count = 1;
-    correct = 0;
-    incorrect = 0;
-    while count<=amount
-        [label score cost] = predict(model, features(count));
-        if(strcmp(label, "manmade")==1)
-           correct = correct + 1;
-        else
-            incorrect = incorrect + 1;
-        end
-        count = count + 1;
-    end
-    y = correct/(count-1);
+    [label score cost] = predict(model, features);
+    result = strcmp(label,"manmade");
+    sum(result)
+    numel(result)
+    y = sum(result)/numel(result);
 end
 function y = predict_natural(model, features)
-amount = size(features);
-    amount = amount(1);
-    count = 1;
-    correct = 0;
-    incorrect = 0;
-    while count<=amount
-        [label score cost] = predict(model, features(count));
-        if(strcmp(label, "natural")==1)
-           correct = correct + 1;
-        else
-            incorrect = incorrect + 1;
-        end
-        count = count + 1;
-    end
-    y = correct/(count-1);
+[label score cost] = predict(model, features);
+    result = strcmp(label,"natural");
+    sum(result)
+    numel(result)
+    y = sum(result)/numel(result);
 end
--- knn_preTrainning.m
+++ knn_preTrainning.m
@@ -4,7 +4,7 @@ function features = knn_preTrainning()
   features.generate_labels = @labels;
   features.load_features_from_file = @load_features_from_file;
 end
-function y = gabor_feature(image_folder)
+function y = gabor_feature(image_folder,name_of_the_file)
     %Create the gabor filters
     rot = [40 70 100 130 160]; % we have four orientations
     RF_siz    = 4;
@@ -37,12 +37,12 @@ function y = gabor_feature(image_folder)
         gabor_features(counter)=sum(sum(sum(outMag,4)));
         counter=counter+1;
     end
-    dlmwrite("./modified/gabor.data", gabor_features(1:counter-1,1), 'precision', 8);
+    dlmwrite("./modified/"+name_of_the_file, gabor_features(1:counter-1,1), 'precision', 8);
     y = gabor_features(1:counter-1,:);
 end
-function y = edge_feature(image_folder)    
+function y = edge_feature(image_folder,name_of_the_file)    
     images = dir(image_folder);
-    edge_features = zeros(numel(images), 1);
+    edge_features = zeros(numel(images), 72);
     %Calculate edges and removes everything elese
     fudgeFactor = .5;    
     counter=1;
@@ -51,18 +51,25 @@ function y = edge_feature(image_folder)
         if images(i).isdir
             continue
         end
-        
+        clc
+        sprintf('%d%%',round(i/numel(images)*100))
         %ignore all but selected extensions
         [filepath, name, ext] = fileparts(images(i).name);
         if ~strcmp(ext, '.bmp') && ~strcmp(ext, '.jpg') && ~strcmp(ext, '.png') && ~strcmp(ext, '.JPG')
             continue
         end
         i_name=strcat(image_folder,"/", name, ext);
-        [~, threshold] = edge(rgb2gray(imread(i_name)), 'sobel');
-        edge_features(counter) = sum(sum(edge(rgb2gray(imread(i_name)),'sobel', threshold * fudgeFactor)));
+        [~, threshold] = edge(rgb2gray(imread(i_name)), 'canny');
+        edge_image = edge(rgb2gray(imread(i_name)),'canny', threshold * fudgeFactor);
+        [feature,viz] = extractHOGFeatures(edge_image);
+        idx = feature > 0;
+        %Use to remove the 0 values from the hist
+        [x,y] = histcounts(feature(idx),72);
+        %[x,y] = histcounts(feature,72);
+        edge_features(counter,:) = x;
         counter=counter+1;
     end
-    dlmwrite("./modified/edge.data", edge_features(1:counter-1,1), 'precision', 8);
+    dlmwrite("./modified/" + name_of_the_file, edge_features(1:counter-1,:), 'precision', 8);
     y = edge_features(1:counter-1,:);
 end
 
@@ -71,10 +78,14 @@ function y = labels(image_folder,label)
     counter=1;
     labels{numel(files)} = {};
     for i=2:numel(files)
-        labels{counter} = label;
+        if strcmp(label,"natural")== 1
+            labels{counter} = "natural";
+        else
+            labels{counter} = "manmade";    
+        end
         counter = counter + 1;
     end
-    y = labels(1:numel(files)-2);
+    y = cellstr(labels(1:numel(files)-2));
 end
 
 function y = load_features_from_file(feature_file)
